6bec932970aa7ce66219612e02e335093235a884
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-05 15:40:21 +0300

    add client

---
+++ src/main/java/by/x1ss/springswitch/model/Client.java
@@ -0,0 +1,14 @@
+package by.x1ss.springswitch.model;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Client {
+    private String name;
+    private String task;
+}
