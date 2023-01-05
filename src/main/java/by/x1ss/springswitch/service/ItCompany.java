79102d7adb86c003793801417cabf53d65e9a2e9
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-05 15:40:13 +0300

    add ItCompany

---
+++ src/main/java/by/x1ss/springswitch/service/ItCompany.java
@@ -0,0 +1,7 @@
+package by.x1ss.springswitch.service;
+
+import by.x1ss.springswitch.model.Client;
+
+public interface ItCompany {
+    void processClient(Client client);
+}
---
+++ src/main/java/by/x1ss/springswitch/service/ItCompanyImpl.java
@@ -0,0 +1,20 @@
+package by.x1ss.springswitch.service;
+
+import by.x1ss.springswitch.model.Client;
+import by.x1ss.springswitch.model.employee.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ItCompanyImpl {
+    @Autowired
+    private Map<String, Employee> map;
+
+    public void processClient(Client client) {
+        System.out.println("Processing client " + client.getName());
+        Employee employee = map.getOrDefault(client.getTask(), map.get("default"));
+        employee.processTask(client);
+    }
+}
