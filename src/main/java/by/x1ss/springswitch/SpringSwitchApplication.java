4ad80b7aad571d3b3cfa10574cfa3e88872b7528
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-05 14:37:25 +0300

    init commit

---
+++ src/main/java/by/x1ss/springswitch/SpringSwitchApplication.java
@@ -0,0 +1,12 @@
+package by.x1ss.springswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwitchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/x1ss/springswitch/SpringSwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package by.x1ss.springswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
